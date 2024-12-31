a28f0094084823906c90d9b82d7bf0ea205abf7e
yangzl <yangzl@ucap.com.cn>
2024-12-31 16:45:05 +0800

    qdrant调优参数后评测结果还是离milvus差2倍

--- vectordb_bench/backend/clients/qdrant_cloud/qdrant_cloud.py
+++ vectordb_bench/backend/clients/qdrant_cloud/qdrant_cloud.py
@@ -15,21 +15,20 @@ from qdrant_client.http.models import (
     Range,
 )
 
-from qdrant_client import QdrantClient
-
+from qdrant_client import QdrantClient, models
 
 log = logging.getLogger(__name__)
 
 
 class QdrantCloud(VectorDB):
     def __init__(
-        self,
-        dim: int,
-        db_config: dict,
-        db_case_config: DBCaseConfig,
-        collection_name: str = "QdrantCloudCollection",
-        drop_old: bool = False,
-        **kwargs,
+            self,
+            dim: int,
+            db_config: dict,
+            db_case_config: DBCaseConfig,
+            collection_name: str = "QdrantCloudCollection",
+            drop_old: bool = False,
+            **kwargs,
     ):
         """Initialize wrapper around the QdrantCloud vector database."""
         self.db_config = db_config
@@ -57,11 +56,10 @@ class QdrantCloud(VectorDB):
         self.qdrant_client = QdrantClient(**self.db_config)
         yield
         self.qdrant_client = None
-        del(self.qdrant_client)
+        del (self.qdrant_client)
 
     def ready_to_load(self):
         pass
-
 
     def optimize(self):
         assert self.qdrant_client, "Please call self.init() before"
@@ -74,7 +72,8 @@ class QdrantCloud(VectorDB):
                 if info.status != CollectionStatus.GREEN:
                     continue
                 if info.status == CollectionStatus.GREEN:
-                    log.info(f"Stored vectors: {info.vectors_count}, Indexed vectors: {info.indexed_vectors_count}, Collection status: {info.indexed_vectors_count}")
+                    log.info(
+                        f"Stored vectors: {info.vectors_count}, Indexed vectors: {info.indexed_vectors_count}, Collection status: {info.indexed_vectors_count}")
                     return
         except Exception as e:
             log.warning(f"QdrantCloud ready to search error: {e}")
@@ -84,9 +83,16 @@ class QdrantCloud(VectorDB):
         log.info(f"Create collection: {self.collection_name}")
 
         try:
+            quantization_config = models.ScalarQuantization(
+                scalar=models.ScalarQuantizationConfig(
+                    type=models.ScalarType.INT8,
+                    always_ram=True,
+                ),
+            )
             qdrant_client.create_collection(
                 collection_name=self.collection_name,
-                vectors_config=VectorParams(size=dim, distance=self.case_config.index_param()["distance"])
+                vectors_config=VectorParams(size=dim, distance=self.case_config.index_param()["distance"]),
+                quantization_config=quantization_config
             )
 
             qdrant_client.create_payload_index(
@@ -102,10 +108,10 @@ class QdrantCloud(VectorDB):
             raise e from None
 
     def insert_embeddings(
-        self,
-        embeddings: list[list[float]],
-        metadata: list[int],
-        **kwargs,
+            self,
+            embeddings: list[list[float]],
+            metadata: list[int],
+            **kwargs,
     ) -> (int, Exception):
         """Insert embeddings into Milvus. should call self.init() first"""
         assert self.qdrant_client is not None
@@ -115,7 +121,7 @@ class QdrantCloud(VectorDB):
             for offset in range(0, len(embeddings), QDRANT_BATCH_SIZE):
                 vectors = embeddings[offset: offset + QDRANT_BATCH_SIZE]
                 ids = metadata[offset: offset + QDRANT_BATCH_SIZE]
-                payloads=[{self._primary_field: v} for v in ids]
+                payloads = [{self._primary_field: v} for v in ids]
                 _ = self.qdrant_client.upsert(
                     collection_name=self.collection_name,
                     wait=True,
@@ -128,11 +134,11 @@ class QdrantCloud(VectorDB):
             return len(metadata), None
 
     def search_embedding(
-        self,
-        query: list[float],
-        k: int = 100,
-        filters: dict | None = None,
-        timeout: int | None = None,
+            self,
+            query: list[float],
+            k: int = 100,
+            filters: dict | None = None,
+            timeout: int | None = None,
     ) -> list[int]:
         """Perform a search on a query embedding and return results with score.
         Should call self.init() first.
@@ -143,8 +149,8 @@ class QdrantCloud(VectorDB):
         if filters:
             f = Filter(
                 must=[FieldCondition(
-                    key = self._primary_field,
-                    range = Range(
+                    key=self._primary_field,
+                    range=Range(
                         gt=filters.get('id'),
                     ),
                 )]
@@ -155,6 +161,7 @@ class QdrantCloud(VectorDB):
             query_vector=query,
             limit=k,
             query_filter=f,
+            search_params=models.SearchParams(hnsw_ef=128, exact=False)
             #  with_payload=True,
         ),
 
